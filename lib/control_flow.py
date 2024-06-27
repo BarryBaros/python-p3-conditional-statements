ff6de61a462c6b4c9f2c7c5ef29d88a8edadb232
Barasa <timbaros87@gmail.com>
2024-06-27 16:48:16 +0300

    Admin Login

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -2,6 +2,12 @@
 
 def admin_login(username, password):
     # your code here
+    if (username == "admin" or username == "ADMIN") and password == "12345":
+        return "Access granted"
+    else:
+     return "Access denied"
+    
+
     pass
 
 def hows_the_weather(temperature):
